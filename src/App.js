8e511d544604dd2d207e7ba63e2e33fc5c46a483
walshcam <35083171+walshcam@users.noreply.github.com>
2018-05-27 15:56:31 -0400

    Learn Ternary and how to get ternaries to work in the render statement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    show: true
+  }
+
+  toggleEvent = () => {
+    const currentShow = this.state.show;
+    this.setState({
+      show: !currentShow
+    }
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick = {this.toggleEvent}>Show!</button>
+        
+        {
+          this.state.show ?
+        
+        <h1>Hello World</h1> : null
+
+        }
+
       </div>
     );
   }
